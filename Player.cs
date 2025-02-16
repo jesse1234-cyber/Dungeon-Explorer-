83d71e8240c2559a83175b5eb334975fa28f3181
Ben Ashby <100142468+BenAshby04@users.noreply.github.com>
2025-02-16 22:33:41 +0000

    Added Comments to Player.cs

--- Player.cs
+++ Player.cs
@@ -2,6 +2,8 @@
 
 namespace DungeonExplorer
 {
+
+    // Player class that inherits from Entity
     public class Player : Entity
     {
       
@@ -9,15 +11,18 @@ namespace DungeonExplorer
 
         public Player(string name, int health) 
         {
+            // Set the player's name and health
             Name = name;
             Health = health;
         }
         public void PickUpItem(Item item)
         {
+            // Add the item to the player's inventory
             inventory.Add(item);
         }
         public string InventoryContents()
         {
+            // Return the contents of the player's inventory
             return string.Join(", ", inventory);
         }
     }
