72cb52448efdb1446a37652eaf573151192f269c
Ben Ashby <100142468+BenAshby04@users.noreply.github.com>
2025-02-16 22:28:08 +0000

    Added Comments to Map.cs

--- Map.cs
+++ Map.cs
@@ -45,6 +45,7 @@ namespace DungeonExplorer
             bool roomMenu = true;
             while (roomMenu)
             {
+                // Display the change room menu
                 Console.WriteLine("Change Room Menu: ");
                 Console.WriteLine("Commands:");
                 Console.WriteLine("1) Right");
@@ -57,45 +58,60 @@ namespace DungeonExplorer
                 direction = Console.ReadLine().ToLower();
                 if (direction == "right")
                 {
+                    // Check if the room to the right exists
                     if (currentX + 1 <= width && layout[currentX +1,currentY] == 1)
                     {
+                        // Return the room to the right
                         return roomLayoutArray[currentX + 1, currentY];
                     }
                 }
                 else if (direction == "left")
                 {
+                    // Check if the room to the left exists
                     if (currentX - 1 >= 0 && layout[currentX -1, currentY] == 1)
                     {
+                        // Return the room to the left
                         return roomLayoutArray[currentX - 1, currentY];
                     }
                 }
                 else if (direction == "up")
                 {
+                    // Check if the room "above" exists
                     if (currentY - 1 >= 0 && layout[currentX, currentY -1] == 1)
                     {
+                        // Return the room "above"
                         return roomLayoutArray[currentX, currentY - 1];
                     }
                 }
                 else if (direction == "down")
                 {
+                    // Check if the room "below" exists
                     if (currentY + 1 <= height && layout[currentX, currentY + 1] == 1)
                     {
+                        // Return the room "below"
                         return roomLayoutArray[currentX, currentY + 1];
                     }
                 }
                 else if (direction == "exit")
                 {
+                    // Exit the room menu
                     roomMenu = false;
                 }
                 else
                 {
+                    // If a command is not recognised
                     Console.WriteLine("Invalid direction");
                 }
+                // If there is no door in the direction the player wants to go
                 Console.WriteLine("There is no door to enter in that direction!");
                 
             }
+            // Return the currentRoom if the player exits the room menu
             return currentRoom;
         }
+        
+
+        // Display the current map
         public void DisplayMap()
         {
             Console.WriteLine("After reading your map you see:");
@@ -103,6 +119,7 @@ namespace DungeonExplorer
             {
                 for (int i = 0; i < height; i++)
                 {
+                    // Displays a O if there is a room and a X if there isn't
                     if (layout[i, j] == 1)
                     {
                         Console.Write("O");
