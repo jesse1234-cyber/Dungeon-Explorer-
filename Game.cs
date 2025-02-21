3c44634c60a87d3b73ae552720131c376cba2fa3
EksDeeEksDee <150486575+EksDeeEksDee@users.noreply.github.com>
2025-02-21 14:25:06 +0000

    Edited development.
    Changed the files so that they do what they're supposed to.

--- Game.cs
+++ Game.cs
@@ -5,21 +5,84 @@ namespace DungeonExplorer
 {
     internal class Game
     {
-        private Player player;
-        private Room currentRoom;
+        private Player player = new Player();
+        private Room currentRoom = new Room();
 
         public Game()
         {
-            // Initialize the game with one room and one player
+            Console.Write("Enter the player name: ");
+            string new_name = Console.ReadLine();
+            player.Name = new_name;
+            player.Health = 100;
+            currentRoom.Description = "The room is dark, the only thing you can see is a little glow in the corner of the room.";
 
         }
         public void Start()
         {
             // Change the playing logic into true and populate the while loop
-            bool playing = false;
+            bool playing = true;
             while (playing)
             {
-                // Code your playing logic here
+                Console.WriteLine("Type help for a list of commands");
+                Console.Write("Enter command: ");
+                string user_input = Console.ReadLine().ToLower();
+                if (user_input == "help")
+                {
+                    Console.WriteLine("Commands:");
+                    Console.WriteLine("Status - displays the players' name, health and inventory.");
+                    Console.WriteLine("Description - displays the description of the room.");
+                    Console.WriteLine("Quit - exits the game.");
+                }
+                else if (user_input == "status")
+                {
+                    Console.WriteLine("Name: " + player.Name);
+                    Console.WriteLine("Health: " + player.Health);
+                    Console.WriteLine("Inventory: " + player.InventoryContents());
+                }
+                else if (user_input == "description")
+                {
+                    Console.WriteLine(currentRoom.GetDescription());
+                    Console.Write("Do you want to inspect the glowing thing? (Y/N): ");
+                    string choice_1 = Console.ReadLine().ToUpper();
+                    if (choice_1 == "Y")
+                    {
+                        Console.Write("You inspect the glow, it turns out to be a key. Do you want to pick up the key? (Y/N): ");
+                        string choice_2 = Console.ReadLine().ToUpper();
+                        if (choice_2 == "Y")
+                        {
+                            player.PickUpItem("Key");
+                            Console.WriteLine("Player Inventory: " + player.InventoryContents());
+
+                        }
+                        else if (choice_2 == "N")
+                        {
+                            Console.WriteLine("You decide not to pick up the key.");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input.");
+                        }
+                    }
+                    else if (choice_1 == "N")
+                    {
+                        Console.WriteLine("You decide not to inspect the glowing thing.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input.");
+                    }
+
+                }
+                else if (user_input == "quit")
+                {
+                    Console.WriteLine("Exiting....");
+                    playing = false;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                }
             }
         }
     }
--- Player.cs
+++ Player.cs
@@ -1,21 +1,47 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace DungeonExplorer
 {
     public class Player
     {
-        public string Name { get; private set; }
-        public int Health { get; private set; }
+        private string name;
+        private int health;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Player name cannot be empty.");
+                }
+                else
+                {
+                    name = value;
+                }
+            }
+        }
+        public int Health
+        {
+            get { return health; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Player health cannot be empty.");
+                }
+                else
+                {
+                    health = value;
+                }
+            }
+        }
         private List<string> inventory = new List<string>();
 
-        public Player(string name, int health) 
-        {
-            Name = name;
-            Health = health;
-        }
         public void PickUpItem(string item)
         {
-
+            inventory.Add(item);
         }
         public string InventoryContents()
         {
