fbdfd6a14682007facdfeca5931a32109bb772cc
Tasha53 <tashabelle11@icloud.com>
2025-02-23 20:41:32 +0000

    Update Game.cs
    Instantiated room object. Program randomly selects one of four different rooms and randomly determines if there is a monster in the room.

--- Game.cs
+++ Game.cs
@@ -8,12 +8,14 @@ namespace DungeonExplorer
     {
         private Player player1;
         private Room currentRoom;
+        static Random rnd = new Random();
 
         public Game()
         {
             // Initialize the game with one room and one player
 
             player1 = new Player("", 0, new List<string>());
+            currentRoom = new Room("", false);
         }
         public void Start()
         {
@@ -32,6 +34,39 @@ namespace DungeonExplorer
                 player1.Inventory = new List<string>();
 
                 Console.WriteLine($"Player's name is: {player1.Name} \n{player1.Name}'s health is: {player1.Health} \nInventory is empty\nPress any key to begin your adventure...");
+                Console.ReadKey();
+
+                int roomNum = rnd.Next(1, 4);
+                switch (roomNum)
+                {
+                        case 1:
+                            currentRoom.Description = "You enter a small, dimly lit room. There is a small health potion placed on a table in the far left corner.";
+                            break;
+                        case 2:
+                            currentRoom.Description = "You enter a completely dark room. It is impossible to see anything. Your fumble around in the darkness searching for a door to the next room.";
+                            break;
+                        case 3:
+                            currentRoom.Description = "You enter a large, brightly lit room. There is a large health potion hidden under a cloth in the near left corner of the room.";
+                            break;
+                        case 4:
+                            currentRoom.Description = "You emerge into blinding sunlight. You have escaped the dungeon!";
+                            break;
+                }
+                int monsterPresent = rnd.Next(1, 2);
+                if (monsterPresent == 1)
+                {
+                    currentRoom.Monster = true;
+                    Console.WriteLine("There is a monster");
+                    Console.ReadKey();
+                }
+                    else if (monsterPresent == 2)
+                {
+                    currentRoom.Monster = false;
+                    Console.WriteLine("There is no monster");
+                    Console.ReadKey();
+                }
+
+                Console.WriteLine(currentRoom.Description);
                 Console.ReadKey();
             }
         }
