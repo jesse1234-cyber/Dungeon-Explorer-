af542cd8564e900836cdfb72d7c2731a26522a23
Tasha53 <tashabelle11@icloud.com>
2025-02-23 20:46:51 +0000

    Update Room.cs
    created game object for room

--- Room.cs
+++ Room.cs
@@ -3,12 +3,37 @@
     public class Room
     {
         private string description;
+        private bool monster;
+        static Random rnd = new Random();
 
-        public Room(string description)
+        public string Description
         {
-            this.description = description;
+            get { return description; }
+            set { description = value; }
         }
 
+         public bool Monster
+         {
+             get { return monster; }
+             set
+             {
+                 int randomNum = rnd.Next(1, 2);
+                 if (randomNum == 1)
+                 {
+                     monster = true;
+                 }
+                 else if (randomNum == 2) 
+                 {
+                    monster = false;
+                 }
+             }
+         }
+
+            public Room(string description)
+            {
+                description = Description;
+                monster = Monster;
+            }
         public string GetDescription()
         {
             return description;
