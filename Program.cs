0512460b75de879d7ab9d79fa47792a7e2a3be0c
elsey10 <williamelsey06@outlook.com>
2025-02-24 17:50:42 +0000

    Update Program.cs
    Update to Program.cs, adds Player, Rooms and Stats

--- Program.cs
+++ Program.cs
@@ -12,7 +12,19 @@ namespace DungeonExplorer
         {
             Game game = new Game();
             game.Start();
-            Console.WriteLine("Waiting for your Implementation");
+            Console.WriteLine("Please input your name:");
+            //Allows an input from the user to take there name. Stores it as variable named "userName"
+            string userName = Console.ReadLine();
+            //Instantiates a player with the name user, uses "userName" variable as the Name for the player. 
+            Player user = new Player(userName, 15);
+            Console.WriteLine("Hello " + user.Name);
+            Console.WriteLine("Player Statistics:" + "\n" + "Health: " + user.Health);
+
+
+            //Create a room with name Kitchen and description of Kitchen 
+            Room kitchen = new Room("A cold room. The air is heavy and moist, with a distinct smell of Sewage Water. The walls and floor are both made from old cracked stones, and the floor a deep brown rotting wood of some kind. There is a large metallatic table positioned in the centre of the room, potentially a surgical table of some type. There are spots of dried blood painting the floor that surrounded the table.");
+            //Runs get description for object Kitchen to prints its description to Console. 
+            Console.WriteLine(kitchen.GetDescription());
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
         }
