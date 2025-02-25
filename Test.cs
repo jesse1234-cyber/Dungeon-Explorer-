b25124a3381c58fbcafe18479065e8d733f6cbdb
Ben Ashby <100142468+BenAshby04@users.noreply.github.com>
2025-02-25 19:37:27 +0000

    Added Test.cs

---
+++ Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    class Test
+    {
+        
+        public Test()
+        {
+
+        }
+        public void TestCase(bool testing)
+        {
+            Debug.Assert(testing, "Test failed");
+        }
+    }
+}
