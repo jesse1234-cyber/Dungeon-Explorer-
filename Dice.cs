f24e0362087bb02338614f2138ab8f8a5058ca4d
magicalpapaya58717 <146379970+magicalpapaya58717@users.noreply.github.com>
2025-03-04 09:51:21 +0000

    Commentary on dice

--- Dice.cs
+++ Dice.cs
@@ -6,6 +6,14 @@ using System.Threading.Tasks;
 
 namespace DungeonCrawler
 {
+    /// <summary>
+    /// Dice is a class that has one attribute only, namely the number of its
+    /// faces. It's single function, Roll, produces a random integer from 1 to
+    /// the number of faces it has. Throughout my code these virtual dice are typically
+    /// labelled Dx where x stands for the number of its faces, so D6 or D20 for example. 
+    /// Dice form the basis for combat and a lot of other random events in the game. 
+    /// They are also important in character creation.
+    /// </summary>
     public class Dice
     {
         int faces { get; set; }
