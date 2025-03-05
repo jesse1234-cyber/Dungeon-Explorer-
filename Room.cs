3dbb2e92b5a8cf6017bd67392dd54930e462445d
Tasha53 <tashabelle11@icloud.com>
2025-03-05 11:58:57 +0000

    Update Room.cs

--- Room.cs
+++ Room.cs
@@ -3,37 +3,12 @@
     public class Room
     {
         private string description;
-        private bool monster;
-        static Random rnd = new Random();
 
-        public string Description
+        public Room(string description)
         {
-            get { return description; }
-            set { description = value; }
+            this.description = description;
         }
 
-         public bool Monster
-         {
-             get { return monster; }
-             set
-             {
-                 int randomNum = rnd.Next(1, 2);
-                 if (randomNum == 1)
-                 {
-                     monster = true;
-                 }
-                 else if (randomNum == 2) 
-                 {
-                    monster = false;
-                 }
-             }
-         }
-
-            public Room(string description)
-            {
-                description = Description;
-                monster = Monster;
-            }
         public string GetDescription()
         {
             return description;
