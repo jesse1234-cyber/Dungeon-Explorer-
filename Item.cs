e64117cea07468953cc2bb4ab336bb15de760cf0
Vladdy <182237916+Vladikusss@users.noreply.github.com>
2025-03-06 12:46:43 +0000

    Created a new file to manage items from inventory
    This file is yet to be expanded

---
+++ Item.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace DungeonExplorer
+{
+    public enum ItemType { HealSpell, IgnoranceSpell, EliminationSpell, Weapon}
+    public enum Rarity { Common, Rare, Legendary }
+    
+    public class Item
+    {
+    
+    }
+}
