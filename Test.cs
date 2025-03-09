8b2ef5c23ab7c14a7b94b02b70759e642d82a85d
HotGiraffe77 <170359653+HotGiraffe77@users.noreply.github.com>
2025-03-09 19:51:18 +0000

    Fixed some errors

--- Test.cs
+++ Test.cs
@@ -19,7 +19,7 @@ namespace DungeonExplorer
             TestPlayer = new Player("Username", 10);
             Debug.Assert(TestPlayer.Name == "Username", "Player name should be Username.");
             // Tests that the inventory starts empty.
-            Debug.Assert(TestPlayer.InventoryContents() == "Nothing :(", "A new players inventory should be empty.")
+            Debug.Assert(TestPlayer.InventoryContents() == "Nothing :(", "A new players inventory should be empty.");
             // Tests that the items picked up are moved to the inventory.
             TestPlayer.PickUpItem("Health Potion");
             Debug.Assert(TestPlayer.InventoryContents().Contains("Health Potion"), "Inventory should have a health potion.");
