e680cabd997d982e215cf57b43baf1bf83ace5f1
dearly-beloved <137338988+dearly-beloved@users.noreply.github.com>
2025-03-10 20:56:04 +0000

    Changed layout (again lol), changed Room class and updated SetItems method.

--- Room.cs
+++ Room.cs
@@ -1,39 +1,54 @@
 ﻿using System.Collections.Generic;
 
-namespace DungeonExplorer {
-    public class Room {
+namespace DungeonExplorer
+{
+    public class Room
+    {
         private string description;
         private List<string> items;
-        private string roomName;
+        private readonly string roomName;
 
         public Room(string roomName, string description, List<string> items) {
             this.roomName = roomName;
-            this.SetDescription(roomName);
-            this.items = new List<string>();
+            this.SetDescription(description);
+            this.SetItems(items);
         }
+
         public string GetRoomName() {
             return this.roomName;
         }
 
-        public string GetDescription() {
-            return this.description;
-        }
         private void SetDescription(string description) {
             if (description != null) {
                 this.description = description;
             }
         }
 
-        private void AddItems(string roomName) {
-            if (roomName == "Library") {
-                this.items.Add("Spell Book");
-                this.items.Add("Mysterious Potion");
-                this.items.Add("Bone Key");
+        public string GetDescription() {
+            return this.description;
+        }
+
+        public void SetItems(List<string> items) {
+            if (items != null && items.Count > 0) {
+                items.ForEach(item => {
+                    this.AddItem(item);
+                });
+            }
+            else {
+                this.items = new List<string>();
             }
         }
 
-        private void RemoveItems(string item) {
+        public void AddItem(string item) {
+            this.items.Add(item);
+        }
+
+        public void RemoveItem(string item) {
             this.items.Remove(item);
+        }
+
+        public bool DoItemsExist() {
+            return items != null && items.Count > 0;
         }
     }
 }
