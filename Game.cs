50fafa80e04771bf03057e58cf89b19e94b07cc8
elsey10 <williamelsey06@outlook.com>
2025-03-10 12:29:07 +0000

    Update Game
    Added actual Controls and the logic to use the controls

--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.Eventing.Reader;
 using System.Media;
 using Microsoft.Win32;
 
@@ -12,20 +13,42 @@ namespace DungeonExplorer
         public Game(string userName)
         {
             player = new Player(userName, 15);
-            currentRoom = new Room("Kitchen.");
+            currentRoom = new Room("A kitchen. There is a knife resting on the counter.");
         }
         public void Start()
         {
             // Change the playing logic into true and populate the while loop
             bool playing = true;
+            Console.WriteLine("Q - Show Stats.  F - Show Inventory.  E - Show Room Description. G - Pick up Item. R - Quit Game.");
             while (playing)
             {
-                Console.WriteLine("Welcome " + player.Name);
-                Console.WriteLine("Player Stastictics:" + "\n" + "Health: " + player.Health);
-                Console.WriteLine(currentRoom.GetDescription());
-                player.PickUpItem("Jar");
-                Console.WriteLine(player.InventoryContents());
-                playing = false;
+                var input = Console.ReadKey().Key;
+                if (input == ConsoleKey.Q)
+                {
+                    Console.WriteLine(player.Name + " Statistics:" + "\n" + "Health: " + player.Health);
+                }
+                else if (input == ConsoleKey.F)
+                {
+                    Console.WriteLine("Inventory:");
+                    Console.WriteLine(player.InventoryContents());
+                }
+                else if (input == ConsoleKey.E)
+                {
+                    Console.WriteLine(currentRoom.GetDescription());
+                }
+                else if (input == ConsoleKey.G)
+                {
+                    Console.WriteLine("Picked up Knife");
+                    player.PickUpItem("Knife");
+                }
+                else if (input == ConsoleKey.R)
+                {
+                    playing = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please input a valid control");
+                }
             }
         }
     }
