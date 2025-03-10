c61eb0e8d221e9dd3c5af61d9f5206098523f1f4
Borisz Kantor <29576302@students.lincoln.ac.uk>
2025-03-10 19:43:51 +0000

    Added comments

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace DungeonExplorer
             Room startingRoom = new Room(null, new Potion("Health(10) Potion", 0, 10, 0), new Weapon("Sword", 10));
             // Initializing and starting game
             Game game = new Game(player, startingRoom);
+            Console.WriteLine("========Welcome to DUNGEON EXPLORER!========"); // Game title
             game.Start();
             Console.ReadKey();
         }
