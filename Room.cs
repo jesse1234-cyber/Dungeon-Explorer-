5cd0d44e749127a8d95d988418e9c6328dbd21c9
Shariq Ashraf <hamzabashir719@gmail.com>
2025-03-10 22:32:57 +0500

    Update Room.cs

--- Room.cs
+++ Room.cs
@@ -1,17 +1,16 @@
-﻿namespace DungeonExplorer
-{
-    public class Room
-    {
-        private string description;
+﻿class Room:
+    def __init__(self, description, item=None):
+        self.__description = description
+        self.__item = item  # Item available in the room
 
-        public Room(string description)
-        {
-            this.description = description;
-        }
+    def get_description(self):
+        """Returns the room's description."""
+        return self.__description
 
-        public string GetDescription()
-        {
-            return description;
-        }
-    }
-}
+    def get_item(self):
+        """Returns the item in the room (if any)."""
+        return self.__item
+
+    def remove_item(self):
+        """Removes the item from the room."""
+        self.__item = None
