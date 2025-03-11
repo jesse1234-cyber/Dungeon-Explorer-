b74abe2679f4aca63c3ddb7d868720a42213692b
PaigeFr <paige.francome0@gmail.com>
2025-03-11 10:44:54 +0000

    Update Room.cs

--- Room.cs
+++ Room.cs
@@ -1,34 +1,47 @@
-using System.Collections.Generic;
-using System.Runtime.InteropServices.Marshalling;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
+using System;
+using System.Diagnostics;
 
 namespace DungeonExplorer
 {
-    public class Room
+    /// <summary>
+    /// This is a testing class that uses 'debug.assert' to verify aspcts of the code.
+    /// </summary>
+    internal class Tests
     {
-        private string _description;
-        public string RoomName { get; private set; }
-        private List<string> _items;
-
-        public Room(string roomName)
+        public static void RunTests()
         {
-            this.RoomName = roomName;
-            this._items = new List<string>();
-            _items.Add("torch");
-
-            if (roomName == "Cell")
-            this._description = "A dark cell with a bed.";
+            TestPlayer();
+            TestPickUpItem();
+            TestPickupSword();
+            Console.WriteLine("All tests completed. :)");
         }
 
-        public string GetDescription()
+        // Tests for the player initialization  
+        private static void TestPlayer()
         {
-            return $"{RoomName}: {_description}";
+            Player testPlayer = new Player("Player", 100);
+            Debug.Assert(testPlayer.Name == "Player", "The player's name is not set correctly.");
+            Debug.Assert(testPlayer.Health == 100, "The player's health is not set to 100.");
         }
 
-        public List<string> GetItems()
+        // Tests for PickupItem()
+        private static void TestPickUpItem()
         {
-            return _items;
+            Player testPlayer = new Player("Player", 100);
+            testPlayer.PickUpItem("torch");
+            Debug.Assert(testPlayer.InventoryContents().Contains("torch"), "Item pickup has failed.");
+        }
+
+        // Tests for PickUpSword()
+        private static void TestPickupSword()
+        {
+            Game testGame = new Game();
+            Player testPlayer = new Player("Player", 100);
+            Room testRoom = new Room("Room of Doom", "A huge room with bats.");
+            testRoom.AddItem("sword");
+            Debug.Assert(testRoom.ContainsItem("sword"), "The sword should be in Room of Doom.");
+            testPlayer.PickUpItem("sword");
+            Debug.Assert(testPlayer.InventoryContents().Contains("sword"), "The player should have picked up the sword.");
         }
     }
 }
