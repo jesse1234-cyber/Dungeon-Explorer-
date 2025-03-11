a7685fbe26b4744a6ea8eaf306699567234d1e8f
jakub041005 <jakubradziwon22@gmail.com>
2025-03-11 14:46:26 +0000

    post review with comments
    i accidently deleted my comments for my code in the previous version so this one has the added comments to explain whats happening in the code

--- Game.cs
+++ Game.cs
@@ -5,17 +5,17 @@ namespace DungeonExplorer
 {
     internal class Game
     {
-        private Player player;
-        private Room currentRoom;
-        private Random random;
-        private List<Room> rooms;
-        private List<string> items;
-        private List<string> charNames;
+        private Player player; // Player Character
+        private Room currentRoom; // The current room that the player is in
+        private Random random; // Random number generator
+        private List<Room> rooms; // A list with all the rooms that are available
+        private List<string> items; // A list of all the items that are available
+        private List<string> charNames; // A list of names
 
         public Game()
         {
             random = new Random();
-
+            // A list of names the game can choose from if the player does not want to input their own character name
             charNames = new List<string>
             {
                 "John", "Jake", "Mathew", "Dan", "Mike", "Blake", "Morgan", "Sebastian"
@@ -25,23 +25,29 @@ namespace DungeonExplorer
             while (true)
             {
                 try
-                {
+                {   // Displays the game title
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("========================================");
                     Console.WriteLine("           DUNGEON EXPLORER ");
                     Console.WriteLine("========================================\n");
                     Console.ResetColor();
+
+                    // Asks the player if they want to name their character
+
                     Console.WriteLine("Do you wish to name your character? (Yes|No)");
                     string input = Console.ReadLine()?.Trim().ToLower();
 
                     if (input == "yes")
                     {
                         Console.WriteLine("Please name your character:");
-                        playerName = Console.ReadLine();
+                        playerName = Console.ReadLine(); // Sets player name to the user input
                         break;
                     }
                     else if (input == "no")
-                    {
+                    {   
+
+                        // Choose from the list of basic names, if player chooses no to naming their character
+
                         playerName = charNames[random.Next(charNames.Count)];
                         Console.WriteLine($"Your character's name is {playerName}.");
                         break;
@@ -56,10 +62,15 @@ namespace DungeonExplorer
                     Console.WriteLine(ex.Message);
                 }
             }
+            // Player starts off with 100 health
 
             player = new Player(playerName, 100);
+            
+            // List of all the possible items
 
             items = new List<string> { "Health Potion", "Gas Mask", "Rusty Sword", "Broken Shield", "A pile of bones", "Gold Bar" };
+
+            // List of all rooms, with description with a random item each time player enters room
 
             rooms = new List<Room>
             {
@@ -70,6 +81,8 @@ namespace DungeonExplorer
                 new Room("\nA vast, black pit yawns before you, with strange sounds rising from its depths, as though something is waiting.", items[random.Next(items.Count)])
             };
 
+            // Set the current room to a random room from the list
+
             currentRoom = rooms[random.Next(rooms.Count)];
         }
 
@@ -77,13 +90,18 @@ namespace DungeonExplorer
         {
             bool playing = true;
             while (playing)
-            {
+            {   
+
+                // Displays the players stats, inventory and what room they are in
+
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"Player: {player.Name}, Health: {player.Health}");
                 Console.ResetColor();
                 Console.WriteLine("\nInventory: " + player.InventoryContents());
                 Console.WriteLine($"\nCurrent Room: {currentRoom.GetDescription()}");
                 Console.WriteLine($"\nThere is a {currentRoom.Item} in this room.");
+                
+                // Loop that asks the player if they want to pick up the item in the current room
 
                 while (true)
                 {
@@ -113,6 +131,8 @@ namespace DungeonExplorer
                     }
                 }
 
+                // Loop that asks the player if they want to continue playing the game and move onto the next room
+
                 while (true)
                 {
                     try
@@ -129,6 +149,7 @@ namespace DungeonExplorer
                         else if (continueInput == "yes")
                         {
                             Room newRoom;
+                            // If user wants to continue it chooses a random room from the list and moves them
                             do
                             {
                                 newRoom = rooms[random.Next(rooms.Count)];
