8377afaefcc66926ed23b749b716b97bbdc4ba2f
Jas-mine0 <JRitchie10582@gmail.com>
2025-03-11 03:08:29 +0000

    Add files via upload
    11/03/2025 DungeonExplorer commit.

--- Program.cs
+++ Program.cs
@@ -3,18 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Dungeon_Explorer.Program;
 
-namespace DungeonExplorer
+namespace Dungeon_Explorer
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+
+        public class Game
         {
-            Game game = new Game();
-            game.Start();
-            Console.WriteLine("Waiting for your Implementation");
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
+            public static void Main(string[] args)
+            {
+                {
+                    Player player = new Player();
+                    Console.WriteLine("The player's name is: " + player.Player_Name);
+                    // The code below is to create a room.
+                    Room room = new Room();
+                }
+            }
+        }
+
+        public class Player
+        {
+            public string Player_Name;
+
+            public Player()
+            {
+
+                Console.WriteLine("Please enter your name: ");
+                Player_Name = Console.ReadLine();
+                Console.WriteLine("Hello, " + Player_Name);
+            }
+
+            public class Inventory<T>
+            {
+                private List<T> items;
+
+                public Inventory()
+                {
+                    items = new List<T>();
+
+                    Console.WriteLine("Inventory stuff: ");
+                    foreach (var item in items)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            }
+        }
+
+
+        public class Room
+        {
+            public string Room_Description = "This is some test text.";
+            public string Item { get; set; } = "Apple.";
+
+            public Room()
+            {
+                Console.WriteLine("Room description: " + Room_Description);
+                Console.WriteLine("You have picked up an item: Apple.");
+            }
+
+            // public static void Main(string[] args)
+            // {
+            // Room room = new Room();
+            // }
         }
     }
 }
