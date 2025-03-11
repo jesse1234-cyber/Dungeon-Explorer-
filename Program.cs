52f5a525829ae106fe3295f3f519e9c8a7701f20
Borisz Kantor <29576302@students.lincoln.ac.uk>
2025-03-11 16:03:32 +0000

    Added short intro and ability to customize player name.

--- Program.cs
+++ Program.cs
@@ -10,14 +10,20 @@ namespace DungeonExplorer
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("========Welcome to DUNGEON EXPLORER!========\n"); // Game title
             // Initializing player
-            string playerName = "Player";
+            Console.WriteLine("You wake up alone in a dark dungeon. You don't remember who you are or how you got here.");
+            string playerName = "";
+            while (playerName == "")
+            {
+                Console.Write("What will you call yourself?\n>");
+                playerName = Console.ReadLine();
+            }
             Player player = new Player(playerName, 30, 0, 1);
             // Initializing starting room
             Room startingRoom = new Room(null, new Potion("Health(10) Potion", 0, 10, 0), new Weapon("Sword", 10));
             // Initializing and starting game
             Game game = new Game(player, startingRoom);
-            Console.WriteLine("========Welcome to DUNGEON EXPLORER!========\n"); // Game title
             /*
             Test test = new Test();
             test.TestHealth();
