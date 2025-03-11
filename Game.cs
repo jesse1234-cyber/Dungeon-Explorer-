05d04a9971cd65afb50556229a13d5b1c1bbc1ec
Pilumins <109856579+Pilumins@users.noreply.github.com>
2025-03-11 18:47:51 +0000

    Game.cs finished no review
    game.cs file completed not reviewed

--- Game.cs
+++ Game.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Media;
 
 namespace DungeonExplorer
 {
@@ -12,16 +11,57 @@ namespace DungeonExplorer
         {
             // Initialize the game with one room and one player
             player = new Player("Player");
-            currentRoom = new Room("")
+            currentRoom = new Room("You are in a dungeon.There is a sword on the ground near by.");
+            currentRoom.AddItem("sword"); // creates a sword.
 
         }
         public void Start()
         {
-            // Change the playing logic into true and populate the while loop
-            bool playing = false;
+            Console.WriteLine("Dungeon Explorer"); //displays the title of the game.
+            bool playing = true; // this will check to see if the game is still running
             while (playing)
             {
-                // Code your playing logic here
+              // Shows the current room info
+              Console.WriteLine(currentRoom.GetDescription());
+              Console.WriteLine("Items in room: " + currentRoom.ListItems());
+
+              // make the player choose an option
+              Console.WriteLine("Pick one of these options below");
+              Console.WriteLine("Options: 1.'pick up [item]', 'inventory', 'move [direction]', 'exit'");
+
+                // player input
+                string input = Console.ReadLine().ToLower();
+
+                // this will process the player input
+                if (input.StartsWith("pickup"))
+                {
+                    string item = input.Substring(8).Trim();
+                    if (currentRoom.ListItems().Contains(item))
+                    {
+                        currentRoom.RemoveItem(item); ; // removes item from room.
+                        player.PickUpItem(item); // adds item to players inventory.
+                    }
+                    else
+                    {
+                        Console.WriteLine("[item] is not in this room.");
+                    }
+                }
+                else if (input == "inventory")
+                {
+                    // display the player inv
+                    Console.WriteLine("Your inventory: " + player.InventoryContents());
+                }
+                else if (input == "exit")
+                {
+                    // exit game
+                    Console.WriteLine("Thanks for playing!");
+                    playing = false;
+                }
+                else
+                {
+                    //handle invalid inputs
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
             }
         }
     }
