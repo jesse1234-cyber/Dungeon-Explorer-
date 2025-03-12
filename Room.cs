d302caeab532357f26d5fbbe7e325eaf01f2bd2b
oliwkaq <oliwiab1021@gmail.com>
2025-03-12 18:20:27 +0000

    files yes yay

--- Room.cs
+++ Room.cs
@@ -1,17 +1,35 @@
-﻿namespace DungeonExplorer
+﻿using System;
+
+namespace DungeonExplorer
 {
     public class Room
     {
         private string description;
+        public string RoomItem { get; set; }
+        public Room North { get; set; }
+        public Room East { get; set; }
+        public Room South { get; set; }
+        public Room West { get; set; }
 
         public Room(string description)
         {
             this.description = description;
         }
 
+        public void CreateItem(string item)
+        {
+            RoomItem = item;
+        }
+
         public string GetDescription()
         {
             return description;
         }
+
+        public void RemoveItem()
+        {
+            RoomItem = null;
+        }
     }
 }
+
