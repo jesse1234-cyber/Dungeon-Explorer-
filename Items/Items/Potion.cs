5fc98e1dee01148c3c445e51ea094ec1901fcce8
Haydenwood2005 <28823036@students.lincoln.ac.uk>
2025-03-12 17:06:29 +0000

    Update Potion.cs

--- Items/Items/Potion.cs
+++ Items/Items/Potion.cs
@@ -1,32 +1,34 @@
-using System;
 using DungeonExplorer.Player;
+using System;
 
+// Item to add health to player
 namespace DungeonExplorer.Item.Items {
     public class HealthPotion : Item {
-        private const int HealthRegen = 2;
+        private const int HealthRegen = 3;
 
-        public HealthPotion() : base("Health Potion", "Restores a small amount of health.", true)
+        public HealthPotion() : base("Health Potion", "Regenerates a small amount of health.", true)
         {
             // ID is automatically assigned in the base class constructor
         }
 
-        // Method to use the item on a player.
+
+        // Use item on player
         public override void Use(Player.Player player)
         {
 
             if(!(player.Health >= player.getMaxHealth())) {
-                Console.WriteLine("You drink the health potion and feel rejuvenated!");
-                Console.WriteLine("[+2 HP]");
+                Console.WriteLine("You drink the health potion!");
+                Console.WriteLine("[+3 HP]");
                 player.AddHealth(HealthRegen);
 
-                // remove the item from the player's inventory
+                // remove the potion from posession when used
                 player.RemoveItem(this);
 
 
             }
             else
             {
-                Console.WriteLine("You are already at full health!");
+                Console.WriteLine("You are already at max health.");
             }
         }
     }
