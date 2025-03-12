71ff0b0611211a0e3bf7dbda550802605a274066
Will <williamviziteu@gmail.com>
2025-03-12 14:33:13 +0000

    transferred files from old repo
    old repo wasnt forked properly

--- Room.cs
+++ Room.cs
@@ -1,17 +1,47 @@
-﻿namespace DungeonExplorer
+﻿using System.Collections.Generic;
+
+namespace DungeonExplorer
 {
     public class Room
     {
         private string description;
+        private List<string> items;
+        private List<string> monsters;
 
-        public Room(string description)
+        public Room(string description, List<string> items, List<string> monsters)
         {
             this.description = description;
+            this.items = items;
+            this.monsters = monsters;
         }
-
+        public List<string> GetItems()
+        {
+            return items;
+        }
+        public bool DeleteItem(string item)
+        {
+            return items.Remove(item);
+        }
+        public string ItemsContents()
+        {
+            return string.Join(", ", items);
+        }
+        public string MonstersContents()
+        {
+            return string.Join(", ", monsters);
+        }
         public string GetDescription()
         {
-            return description;
+            string roomText = "This is a " + description + " Room.";
+            if (items.Count != 0)
+            {
+                roomText += "\nContains items:" + ItemsContents();
+            }
+            if (monsters.Count != 0)
+            {
+                roomText += "\nContains monsters:" + MonstersContents();
+            }
+            return roomText;
         }
     }
 }
